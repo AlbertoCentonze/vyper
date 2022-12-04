ed14f54c8f1338e71805e59bdcd51c9a40ff72e5
Alberto Centonze <albicento.ac@gmail.com>
2022-12-04 21:38:02 +0100

    fix: Now the deployer has to be deployed manually for mainnet forks

--- VyperTest.sol
+++ VyperTest.sol
@@ -5,5 +5,9 @@ import {Test} from "forge-std/Test.sol";
 import {VyperDeployer} from "./VyperDeployer.sol";
 
 contract VyperTest is Test {
-	VyperDeployer vyperDeployer = new VyperDeployer(vm);
+	VyperDeployer vyperDeployer;
+
+	function initDeployer() public {
+		vyperDeployer = new VyperDeployer(vm);
+	}
 }
