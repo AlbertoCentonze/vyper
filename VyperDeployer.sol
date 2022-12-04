fdce67876a8cc4d618a18bda8b9eb757baf58b1a
Alberto Centonze <albicento.ac@gmail.com>
2022-12-04 01:01:18 +0100

    feat: Added initial library

---
+++ VyperDeployer.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.13;
+
+import {Vm} from "forge-std/Vm.sol";
+
+contract VyperDeployer {
+    Vm vm;
+
+    constructor(Vm _vm) {
+        vm = _vm;
+    }
+
+    function deployContract(string memory fileName) public returns (address) {
+        string[] memory cmds = new string[](2);
+        cmds[0] = "vyper";
+        cmds[1] = string.concat("src/", fileName, ".vy");
+
+        bytes memory bytecode = vm.ffi(cmds);
+
+        address deployedAddress;
+        assembly {
+            deployedAddress := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+
+        require(
+            deployedAddress != address(0),
+            "VyperDeployer could not deploy contract"
+        );
+
+        return deployedAddress;
+    }
+
+    function deployContract(string memory fileName, bytes calldata args)
+        public
+        returns (address)
+    {
+        string[] memory cmds = new string[](2);
+        cmds[0] = "vyper";
+        cmds[1] = string.concat("src/", fileName, ".vy");
+
+        bytes memory _bytecode = vm.ffi(cmds);
+
+        bytes memory bytecode = abi.encodePacked(_bytecode, args);
+
+        address deployedAddress;
+        assembly {
+            deployedAddress := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+
+        require(
+            deployedAddress != address(0),
+            "VyperDeployer could not deploy contract"
+        );
+
+        return deployedAddress;
+    }
+}
---
+++ VyperTest.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {VyperDeployer} from "./VyperDeployer.sol";
+
+contract VyperTest is Test {
+	VyperDeployer vyperDeployer = new VyperDeployer(vm);
+}
